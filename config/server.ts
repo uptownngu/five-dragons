efd6eb62bd0233d49056f3f9d14d18295c71971b
uptownngu <uptownnguyen@gmail.com>
2025-07-10 18:26:02 +0900

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,11 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'), // 모든 IP에서 접근 가능하도록
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://141.164.52.65:1337'), // 외부 URL 설정
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
